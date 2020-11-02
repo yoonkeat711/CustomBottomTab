d9ee0ccc286d5b4fd805f886a5aee0fdceffcb75
yoonkeat <yoonkeat711@gmail.com>
2020-11-02 10:29:48 +0800

    init project

---
+++ ios/Pods/Target Support Files/Pods-CustomBottomTab-tvOS/Pods-CustomBottomTab-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomBottomTab_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_CustomBottomTab_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-CustomBottomTab/Pods-CustomBottomTab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomBottomTab : NSObject
+@end
+@implementation PodsDummy_Pods_CustomBottomTab
+@end
