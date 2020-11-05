42b961e768d0335d34d5ddfbd6d33cfd32bb95ff
yoonkeat <yoonkeat711@gmail.com>
2020-11-05 13:32:39 +0800

    add animation for bottom tab scroll to hide/show

--- App.js
+++ App.js
@@ -6,49 +6,71 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   StatusBar,
+  ScrollView,
 } from 'react-native';
 import BottomTab from './src/BottomTab';
 import TabItem from './src/BottomTab/TabItem';
 import ChatIcon from './src/assets/chat.png';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
+
+  const [tabBarVisible, setTabBarVisible] = useState(true);
+  const [previousOffset, setPreviousOffset] = useState(0);
+
+  const onScroll = e => {
+    const currentOfffset = e.nativeEvent.contentOffset.y;
+    if (currentOfffset > previousOffset) {
+      setTabBarVisible(false);
+    } else {
+      setTabBarVisible(true);
+    }
+    setPreviousOffset(currentOfffset);
+  }
+
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1}}>
-      <BottomTab 
-      activeTintColor={'blue'}
-      inactiveTintColor={'grey'}
-      initialRouteIndex={0}
-      >
-        <TabItem 
-         icon={ChatIcon}
-         text={'Calls'}
-        />
-        <TabItem 
-         icon={ChatIcon}
-         text={'Chats'}
-         badge={3}
-        />
-         <TabItem 
-         icon={ChatIcon}
-         text={'Calls'}
-        />
-        <TabItem 
-         icon={ChatIcon}
-         text={'Calls'}
-        />
-        <TabItem 
-         icon={ChatIcon}
-         text={'Calls'}
-        />
+      <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView onScroll={e => onScroll(e)} contentContainerStyle={{ height: 1000 }} />
+
+        <BottomTab
+          activeTintColor={'blue'}
+          inactiveTintColor={'grey'}
+          initialRouteIndex={0}
+          tabBarVisible={tabBarVisible}
+        >
+          <TabItem
+            icon={ChatIcon}
+            text={'Calls'}
+          />
+          <TabItem
+            icon={ChatIcon}
+            text={'Chats'}
+            badge={3}
+          />
+          <TabItem
+            icon={ChatIcon}
+            text={'Calls'}
+          />
+          <TabItem
+            icon={ChatIcon}
+            text={'Calls'}
+          />
+          <TabItem
+            icon={ChatIcon}
+            text={'Calls'}
+          />
         </BottomTab>
       </SafeAreaView>
+      </SafeAreaProvider>
     </>
   );
 };
--- src/BottomTab/index.js
+++ src/BottomTab/index.js
@@ -1,18 +1,73 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, Dimensions } from 'react-native';
 import BottomTabContext from './BottomTabContext';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type Props = {
     children: ?TabItem;
     initialRouteIndex: Number,
     activeTintColor: String,
     inactiveTintColor: String,
+    tabBarVisible: Boolean,
 }
 
 const TAB_BAR_HEIGHT = 50;
 
-const BottomTab = ({ children, initialRouteIndex = 0, activeTintColor, inactiveTintColor }: Props) => {
+const BottomTab = ({ children, initialRouteIndex = 0, activeTintColor, inactiveTintColor, tabBarVisible = true }: Props) => {
     const [activeIndex, setActiveIndex] = useState(initialRouteIndex);
+
+    const shouldShowTabBar = tabBarVisible;
+    const [isTabBarHidden, setIsTabBarHidden] = React.useState(!shouldShowTabBar);
+    const [visible] = React.useState(() => new Animated.Value(shouldShowTabBar ? 1 : 0));
+    const { bottom } = useSafeAreaInsets();
+
+    React.useEffect(() => {
+        if (shouldShowTabBar) {
+            const animation = Animated.spring;
+
+            animation(visible, {
+                toValue: 1,
+                useNativeDriver: true,
+                duration: 250,
+            }).start(({ finished }) => {
+                if (finished) {
+                    setIsTabBarHidden(false);
+                }
+            })
+        } else {
+            setIsTabBarHidden(true);
+
+            const animation = Animated.timing;
+
+            animation(visible, {
+                toValue: 0,
+                useNativeDriver: true,
+                duration: 200,
+
+            }).start();
+        }
+    }, [visible, shouldShowTabBar]);
+
+    const [layout, setLayout] = React.useState({
+        height: 0,
+        width: Dimensions.get('window').width,
+    });
+
+    const handleLayout = (e) => {
+
+        const { height, width } = e.nativeEvent.layout;
+
+        setLayout((layout) => {
+            if (height === layout.height && width === layout.width) {
+                return layout;
+            } else {
+                return {
+                    height,
+                    width,
+                }
+            }
+        })
+    }
 
     return (
         <BottomTabContext.Provider
@@ -20,17 +75,29 @@ const BottomTab = ({ children, initialRouteIndex = 0, activeTintColor, inactiveT
                 updateActiveIndex: setActiveIndex,
             }}
         >
-            <View style={[styles.container, {height: TAB_BAR_HEIGHT}]}>
-                {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, {
-                        onFocused: index === activeIndex,
-                        activeTintColor: activeTintColor,
-                        inactiveTintColor: inactiveTintColor,
-                        index: index,
-                    })
-                }
-                )}
-            </View>
+            <Animated.View style={[styles.container, {
+                transform: [
+                    {
+                        translateY: visible.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [layout.height + bottom, 0]
+                        })
+                    }
+                ],
+                height: TAB_BAR_HEIGHT,
+            }]}>
+                <View onLayout={handleLayout} style={{ flex: 1, flexDirection: 'row' }}>
+                    {React.Children.map(children, (child, index) => {
+                        return React.cloneElement(child, {
+                            onFocused: index === activeIndex,
+                            activeTintColor: activeTintColor,
+                            inactiveTintColor: inactiveTintColor,
+                            index: index,
+                        })
+                    }
+                    )}
+                </View>
+            </Animated.View>
         </BottomTabContext.Provider>
     )
 }
